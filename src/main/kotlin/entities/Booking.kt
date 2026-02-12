dc12b19c3c4f7fe974bbc741457ed6162b5ae066
Bushkov Nikita <408338@niuitmo.ru>
2026-02-12 13:34:27 +0300

    refactor: small edit in class Booking

--- src/main/kotlin/entities/Booking.kt
+++ src/main/kotlin/entities/Booking.kt
@@ -2,13 +2,13 @@ package com.samsoeed.entities
 
 import java.time.LocalDateTime
 
-data class Booking(val id: Int, val customerName: String) {
+data class Booking(val id: Int, val customer: User) {
 
     val createdAt: LocalDateTime = LocalDateTime.now()
 
     private var basket: MutableList<Bakery> = mutableListOf()
 
-    constructor(id: Int, customerName: String, basket: MutableList<Bakery>) : this(id, customerName) {
+    constructor(id: Int, customer: User, basket: MutableList<Bakery>) : this(id, customer) {
         this.basket = basket
     }
 
